0600050201e71443b3f1a824cec2d1f796e4d8ba
Jeff Vincent <jeff@wistia.com>
2012-09-25 10:58:53 -0400

    js config update with colon in port integer

--- javascripts/js_config.js
+++ javascripts/js_config.js
@@ -1,13 +1,13 @@
 WistiaDoc = {
   url: "http://wistia.com",
   path: "/newdoc",
-  port: "4000",
+  port: ":4000",
   search_path: "/search",
-  search_port: "9393",
+  search_port: ":9393",
   search_app_url: function() {
-    return WistiaDoc.url + ":" + WistiaDoc.search_port + WistiaDoc.path + WistiaDoc.search_path;
+    return WistiaDoc.url + WistiaDoc.search_port + WistiaDoc.path + WistiaDoc.search_path;
   },
   web_app_url: function() {
-    return WistiaDoc.url + ":" + WistiaDoc.port + WistiaDoc.path;
+    return WistiaDoc.url + WistiaDoc.port + WistiaDoc.path;
   }
 };
