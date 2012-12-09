102fc0e365307d28158bb97f2a1c1888e051db71
Jeff Vincent <jeff@wistia.com>
2012-12-08 22:27:27 -0500

    Implemented Liats color ordering

--- javascripts/devhq.js
+++ javascripts/devhq.js
@@ -10,8 +10,12 @@ function colorPicker(index) {
   }
 }
 
-function randomColor() {
-  return colors[Math.floor(Math.random() * colors.length)];
+function colorEachRow() {
+  for (var j=0; j < verticalTiles; j++) {
+    $(".row" + j).each(function(index) {
+      $(this).css('color', colors[index%colors.length]);
+    });
+  }
 }
 
 function getStart(j) {
@@ -40,53 +44,62 @@ function colorMind(i,j) {
 }
 
 //
-// yellow: #F9D164, accent_blue: #69D0E6, orange: #F8654E, dimBlue: #2C5CA7
+// originalColor: #2C5CA7, colors: [ yellow: #F9D164, accent_blue: #69D0E6, orange: #F8654E ]
 //
 var originalColor = 'rgb(44, 92, 167)';
 var colors = ['rgb(249, 209, 100)','rgb(105, 208, 230)','rgb(248, 101, 78)'];
 
-tiled_wall = $(".tiled_wall");
-one_tile = $(".one_tile:first");
-var headerWidth = tiled_wall.width();
-var headerHeight = tiled_wall.height();
-var tileWidth = one_tile.width() + 6;
-var tileHeight = one_tile.height();
+var tiledWall = $(".tiled_wall");
+var firstTile = $(".one_tile:first");
+var headerWidth = tiledWall.width();
+var headerHeight = tiledWall.height();
+var tileWidth = firstTile.width() + 6;
+var tileHeight = firstTile.height();
 var horizontalTiles = Math.ceil(headerWidth / tileWidth) + 2;
 var arrowWidth = Math.floor(600/tileWidth);
 var padding = Math.floor(($(window).width() - 960)/ 2 / tileWidth) + 2;
 var verticalTiles = Math.floor(headerHeight / tileHeight);
-var $startTile = one_tile.remove();
+var $startTile = firstTile.remove();
 
 for (var i=0; i < horizontalTiles; i++) {
   for (var j=0; j < verticalTiles; j++) {
     var $clone = $startTile.clone();
-    $clone.css({ left: tileWidth * i, top: tileHeight * j, color: colorMind(i,j) });
+    $clone.css({ left: tileWidth * i, top: tileHeight * j });
     if ( !isOutsideArrow(i,j) ) {
-      $clone.addClass('insideArrow');
+      $clone.addClass("insideArrow row" + j);
     }
     if ( j < 3 ) {
       $clone.addClass('openBracket');
     }
-    tiled_wall.append($clone);
+    tiledWall.append($clone);
   }
 }
 
+colorEachRow();
+
 var openBrackets = $('.openBracket');
 openBrackets.each(function() {
-  wrapInner(function() {
-    return "<span class='dimSlash' />";
+  $(this).html(function(i, html) {
+    return html.replace("/", "<span class='dimBracket'>/</span>");
   });
 });
 
+// find the last tile, so we can replace the tiles before it
 var lastTile = $('.insideArrow:last');
+// the last row
 var lastRowTiles = $('.one_tile').filter( function() {
   return lastTile.offset().top == $(this).offset().top;
 });
+
 var lastTileIndex = lastRowTiles.index(lastTile);
+// find the tilesToBeReplaced, and the width of those tiles
 var tilesToBeReplaced = lastRowTiles.filter(":gt(" + (lastTileIndex - 5) + ")").filter(":lt(4)");
 var widthOfTilesToBeReplaced = tilesToBeReplaced.length * tileWidth;
+// anchor on the tile BEFORE the tiles that need replacing
 var tileBeforeReplacement = tilesToBeReplaced.first().prev();
 tilesToBeReplaced.remove();
+
+// now we insert the replacement and style
 var $lefty = $("<div class='lefty'>&lt;/</div>");
 var $righty = $("<div class='righty'>&gt;</div>");
 var $middly = $("<div class='middly'>wistia<span class='devhq_accent_blue'>.</span>developers</div>");
@@ -98,15 +111,13 @@ $sickWrapper.css({
   left: parseInt(tileBeforeReplacement.css('left'), 10),
   top: parseInt(tileBeforeReplacement.css('top'), 10) + tileHeight
 });
+// bump the 'nerds' subtitle to line up
 var subtitleOffset = parseInt($sickWrapper.css('left'), 10) + $lefty.width();
-console.log("newer amt", subtitleOffset);
 $('#devhq_subtitle').css('left', subtitleOffset);
-
 
 $('.one_tile').on('click', function() {
   var $this = $(this);
   var color = $this.css('color');
   var index = colors.indexOf(color);
-  newColor = colorPicker(index);
-  $this.css('color', newColor);
+  $this.css('color', colorPicker(index));
 });
