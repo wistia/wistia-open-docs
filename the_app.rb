d5cfc1f1a7a5757e5e74153261128adcdfe61f37
Jeff Vincent <jeff@wistia.com>
2013-04-03 17:23:46 -0400

    Add 301 redirect for randor URL

--- the_app.rb
+++ the_app.rb
@@ -16,6 +16,7 @@ use Rack::Rewrite do
   r301 %r{/start/?}, "#{$config.basepath}"
   r301 %r{/randor-basics/?}, "#{$config.basepath}/wistia-basics-getting-started"
   r301 %r{/randor-(\w+)/?}, "#{$config.basepath}/wistia-basics-$1"
+  r301 %r{/randor?}, "#{$config.basepath}/wistia-basics"
   r301 "#{$config.basepath}/construct-an-embed-code", "#{$config.basepath}/embed-api"
   rewrite %r{#{$config.basepath}(.*)}, '$1'
 end
