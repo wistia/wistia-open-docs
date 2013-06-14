106fd95e1bdab8eeda400d4b50b6cb34e79e0b19
Jeff Vincent <jeff@wistia.com>
2013-06-14 13:16:45 -0400

    Updates to HAML Converter plugin

--- _plugins/haml_converter.rb
+++ _plugins/haml_converter.rb
@@ -1,39 +1,42 @@
+require 'haml'
+require 'sass'
+
 module Jekyll
-  require 'haml'
-  class HamlConverter < Converter
-    safe true
-    priority :low
+  module Converters
+    class HamlConverter < Converter
+      safe true
+      priority :low
 
-    def matches(ext)
-      ext =~ /haml/i
+      def matches(ext)
+        ext =~ /haml/i
+      end
+
+      def output_ext(ext)
+        ".html"
+      end
+
+      def convert(content)
+        engine = Haml::Engine.new(content)
+        engine.render
+      end
     end
 
-    def output_ext(ext)
-      ".html"
-    end
+    class SassConverter < Converter
+      safe true
+      priority :low
 
-    def convert(content)
-      engine = Haml::Engine.new(content)
-      engine.render
-    end
-  end
+      def matches(ext)
+        ext =~ /sass/i
+      end
 
-  require 'sass'
-  class SassConverter < Converter
-    safe true
-    priority :low
+      def output_ext(ext)
+        ".css"
+      end
 
-     def matches(ext)
-      ext =~ /sass/i
-    end
-
-    def output_ext(ext)
-      ".css"
-    end
-
-    def convert(content)
-      engine = Sass::Engine.new(content)
-      engine.render
+      def convert(content)
+        engine = Sass::Engine.new(content)
+        engine.render
+      end
     end
   end
 end
