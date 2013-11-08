6e88a5afafdf131110ba4bf7a8c0dbce556a4307
Jeff Vincent <jeff@wistia.com>
2013-11-08 12:01:57 -0500

    Change to OnePageNav script
    When inside a section, it should still scroll to top of the section if the nav
    link gets clicked.

--- javascripts/jquery.nav.js
+++ javascripts/jquery.nav.js
@@ -141,41 +141,39 @@
 			var $parent = $link.parent();
 			var newLoc = '#' + self.getHash($link);
 			
-			if(!$parent.hasClass(self.config.currentClass)) {
-				//Start callback
-				if(self.config.begin) {
-					self.config.begin();
-				}
-				
-				//Change the highlighted nav item
-				self.adjustNav(self, $parent);
-				
-				//Removing the auto-adjust on scroll
-				self.unbindInterval();
-				
-				//Scroll to the correct position
-				$.scrollTo(newLoc, self.config.scrollSpeed, {
-					axis: 'y',
-					easing: self.config.easing,
-					offset: {
-						top: -self.config.scrollOffset
-					},
-					onAfter: function() {
-						//Do we need to change the hash?
-						if(self.config.changeHash) {
-							window.location.hash = newLoc;
-						}
-						
-						//Add the auto-adjust on scroll back in
-						self.bindInterval();
-						
-						//End callback
-						if(self.config.end) {
-							self.config.end();
-						}
-					}
-				});
-			}
+      //Start callback
+      if(self.config.begin) {
+        self.config.begin();
+      }
+      
+      //Change the highlighted nav item
+      self.adjustNav(self, $parent);
+      
+      //Removing the auto-adjust on scroll
+      self.unbindInterval();
+      
+      //Scroll to the correct position
+      $.scrollTo(newLoc, self.config.scrollSpeed, {
+        axis: 'y',
+        easing: self.config.easing,
+        offset: {
+          top: -self.config.scrollOffset
+        },
+        onAfter: function() {
+          //Do we need to change the hash?
+          if(self.config.changeHash) {
+            window.location.hash = newLoc;
+          }
+          
+          //Add the auto-adjust on scroll back in
+          self.bindInterval();
+          
+          //End callback
+          if(self.config.end) {
+            self.config.end();
+          }
+        }
+      });
 
 			e.preventDefault();
 		},
